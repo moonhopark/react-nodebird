e87a76262716b11737659bda3219f2b4c8be277d
moonho park <moonho4199@naver.com>
2021-08-13 14:53:32 +0900

    first commit

---
+++ ch1/front/pages/signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => {
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>회원가입</div>
+      </AppLayout>
+    </>
+  );
+};
+
+export default Signup;
