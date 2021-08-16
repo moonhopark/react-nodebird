a5118527006053c830a303d8668ca789b32d7343
moonho park <moonho4199@naver.com>
2021-08-16 18:19:43 +0900

    프로필 페이지

--- ch1/front/pages/profile.js
+++ ch1/front/pages/profile.js
@@ -2,15 +2,30 @@ import React from 'react';
 import Head from 'next/head';
 
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  const followingList = [
+    { nickname: '무노팍' },
+    { nickname: '법멋' },
+    { nickname: '노드버드오피셜' },
+  ];
+  const followerList = [
+    { nickname: '무노팍' },
+    { nickname: '법멋' },
+    { nickname: '노드버드오피셜' },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
