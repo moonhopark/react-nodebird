8bb35900ac7ed8fd6780faddf7fdb812755c7e5b
moonhopark <moonho4199@naver.com>
2022-03-15 16:57:24 +0900

    feat: link avatar, profile

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -20,6 +20,7 @@ import {
   RETWEET_FAILURE,
 } from '../reducers/post';
 import FollowButton from './FollowButton';
+import Link from 'next/link';
 
 const PostCard = ({ post }) => {
   const dispatch = useDispatch();
@@ -125,14 +126,26 @@ const PostCard = ({ post }) => {
             }
           >
             <Card.Meta
-              avatar={<Avatar>{post.Retweet.User.nickname[0]}</Avatar>}
+              avatar={
+                <Link href={`/user/${post.Retweet.User.id}`}>
+                  <a>
+                    <Avatar>{post.Retweet.User.nickname[0]}</Avatar>
+                  </a>
+                </Link>
+              }
               title={post.Retweet.User.nickname}
               description={<PostCardContent postData={post.Retweet.content} />}
             />
           </Card>
         ) : (
           <Card.Meta
-            avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+            avatar={
+              <Link href={`/user/${post.User.id}`}>
+                <a>
+                  <Avatar>{post.User.nickname[0]}</Avatar>
+                </a>
+              </Link>
+            }
             title={post.User.nickname}
             description={<PostCardContent postData={post.content} />}
           />
@@ -149,7 +162,13 @@ const PostCard = ({ post }) => {
               <li>
                 <Comment
                   author={item.User.nickname}
-                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  avatar={
+                    <Link href={`/user/${item.User.id}`}>
+                      <a>
+                        <Avatar>{item.User.nickname[0]}</Avatar>
+                      </a>
+                    </Link>
+                  }
                   content={item.content}
                 />
               </li>
--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,12 +1,13 @@
 import React, { useCallback } from 'react';
 import { Card, Avatar, Button } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
+import Link from 'next/link';
 
 import { logoutRequestAction } from '../reducers/user';
 
 const UserProfile = () => {
   const dispatch = useDispatch();
-  const { me, logOutLoading } = useSelector(state => state.user);
+  const { me, logOutLoading } = useSelector((state) => state.user);
 
   const onLogOut = useCallback(() => {
     dispatch(logoutRequestAction());
@@ -15,24 +16,45 @@ const UserProfile = () => {
   return (
     <Card
       actions={[
-        <div key="twit">
-          짹짹
-          <br />
-          {me.Posts.length}
+        <div key='twit'>
+          <Link href={`/user/${me.id}`}>
+            <a>
+              짹짹
+              <br />
+              {me.Posts.length}
+            </a>
+          </Link>
         </div>,
-        <div key="follwings">
-          팔로잉
-          <br />
-          {me.Followings.length}
+        <div key='follwings'>
+          <Link href={'/profile'}>
+            <a>
+              팔로잉
+              <br />
+              {me.Followings.length}
+            </a>
+          </Link>
         </div>,
-        <div key="followers">
-          팔로워
-          <br />
-          {me.Followers.length}
+        <div key='followers'>
+          <Link href={'/profile'}>
+            <a>
+              팔로워
+              <br />
+              {me.Followers.length}
+            </a>
+          </Link>
         </div>,
       ]}
     >
-      <Card.Meta avatar={<Avatar>{me.nickname[0]}</Avatar>} title={me.nickname} />
+      <Card.Meta
+        avatar={
+          <Link href={`/user/${me.id}`}>
+            <a>
+              <Avatar>{me.nickname[0]}</Avatar>
+            </a>
+          </Link>
+        }
+        title={me.nickname}
+      />
       <Button onClick={onLogOut} loading={logOutLoading}>
         로그아웃
       </Button>
